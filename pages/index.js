f5c9813c4d9165bce7d3a3c48bd7fe1bd1e0ba10
kamonthep <kamonthep026@gmail.com>
2021-03-22 14:23:58 +0700

    Done nextEvents ต่อ api firebase

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
-function HomePage() {
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+
+function HomePage(props) {
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
