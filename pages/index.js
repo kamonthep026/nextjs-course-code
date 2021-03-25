e7a205226bcd0063af351bd1aeed811675695e0e
kamonthep <kamonthep026@gmail.com>
2021-03-25 17:23:36 +0700

    save

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler() {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    const reqBody = { email: enteredEmail, feedback: enteredFeedback };
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" row="5" ref={feedbackInputRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <br />
+      <button>Load Feedback</button>
     </div>
   );
 }
